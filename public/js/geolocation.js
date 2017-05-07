dfe83da5cf4d7bcdd2719314391abdef08cd9c79
esterlign <esterling@brainbytescreative.com>
2017-05-06 20:02:40 -0400

    Sequelize sync added in the server

---
+++ public/js/geolocation.js
@@ -0,0 +1,25 @@
+/**
+ * Created by esterlingaccime on 5/6/17.
+ */
+console.log("Hello World!!");
+var x = document.getElementById("demo");
+
+
+var app = {
+    showPosition: function (position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    },
+
+
+    getLocation: function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+};
+
+
+app.getLocation();
